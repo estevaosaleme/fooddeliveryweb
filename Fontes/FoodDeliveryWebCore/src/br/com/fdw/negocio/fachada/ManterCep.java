1cee79bb09085bd8d259d5f36b8b85d05f7199db
estevaobissoli <estevaobissoli@adc61de8-b1f8-11de-aad8-5f085193a258>
2010-01-26 01:08:17 +0000

    Implementação da fachada para o caso de uso manterCep

--- Fontes/FoodDeliveryWebCore/src/br/com/fdw/negocio/fachada/ManterCep.java
+++ Fontes/FoodDeliveryWebCore/src/br/com/fdw/negocio/fachada/ManterCep.java
@@ -8,12 +8,41 @@ import java.util.List;
 /**
  * métodos devem ser públicos
  * na fachada deve conter os casos de uso
+ * Padrão: nome dos métodos são os nomes dos casos de uso em português
  */
 public class ManterCep {
 
+    private CepProcess cepProcess;
+
+    public ManterCep()
+    {
+        this.cepProcess = new CepProcess();
+    }
+
     public List<Cep> listarCeps()
     {
-        CepProcess cepProcess = new CepProcess();
-        return cepProcess.getAll();
+        return this.cepProcess.getAll();
     }
+    
+    public Cep listarCep(int codigoCep)
+    {
+        return this.cepProcess.getCep(codigoCep);
+    }
+
+    public Cep incluirCep(Cep cep)
+    {
+        return this.cepProcess.setCep(cep);
+    }
+
+    public Cep alterarCep(Cep cep)
+    {
+        return this.cepProcess.updateCep(cep);
+    }
+
+    public boolean excluirCep(int codigoCep)
+    {
+        return this.cepProcess.removeCep(codigoCep);
+    }
+
+
 }
