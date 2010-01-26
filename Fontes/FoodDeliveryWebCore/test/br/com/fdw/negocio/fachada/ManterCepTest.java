685889093d80fb2225fafde70454efc4d86f1369
estevaobissoli <estevaobissoli@adc61de8-b1f8-11de-aad8-5f085193a258>
2010-01-26 01:09:54 +0000

    Teste unitários para o caso de uso ManterCep

--- Fontes/FoodDeliveryWebCore/test/br/com/fdw/negocio/fachada/ManterCepTest.java
+++ Fontes/FoodDeliveryWebCore/test/br/com/fdw/negocio/fachada/ManterCepTest.java
@@ -1,7 +1,7 @@
 
 package br.com.fdw.negocio.fachada;
 
-import java.util.ArrayList;
+import br.com.fdw.negocio.entidades.Cep;
 import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -39,9 +39,45 @@ public class ManterCepTest {
     public void testListarCeps() {
         System.out.println("ListarCeps");
         ManterCep instance = new ManterCep();
-        List expResult = new ArrayList();
         List result = instance.listarCeps();
-        assertEquals(expResult.size(), result.size());
+        assertTrue("Ok", result.size() > 0);
+    }
+
+    @Test
+    public void testListarCep() {
+        System.out.println("ListarCep");
+        ManterCep instance = new ManterCep();
+        Cep result = instance.listarCep(1);
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testIncluirCep() {
+        System.out.println("IncluirCep");
+        ManterCep instance = new ManterCep();
+        Cep cep = new Cep();
+        cep.setNomeBairro("Praia do Canto");
+        cep.setNomeCidade("Vitória");
+        cep.setNomeLogradouro("Rua Aleixo Neto");
+        cep.setNumeroCep("29051-000");
+        cep.setSiglaEstado("ES");
+
+        Cep result = instance.incluirCep(cep);
+
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testAlterarCep() {
+        System.out.println("AlterarCep");
+        ManterCep instance = new ManterCep();
+        Cep cep = instance.listarCep(2);
+        cep.setNomeLogradouro("Rua Chapot Prevot");
+        cep.setNumeroCep("29051-005");
+
+        Cep result = instance.alterarCep(cep);
+
+        assertNotNull(result);
     }
 
 }
