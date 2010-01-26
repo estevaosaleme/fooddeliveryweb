4a1c39785f04be374dd5aa0fb2170cda4b45b504
estevaobissoli <estevaobissoli@adc61de8-b1f8-11de-aad8-5f085193a258>
2010-01-26 01:07:43 +0000

    Business Logic para CEP

--- Fontes/FoodDeliveryWebCore/src/br/com/fdw/negocio/processo/CepProcess.java
+++ Fontes/FoodDeliveryWebCore/src/br/com/fdw/negocio/processo/CepProcess.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 /**
  * Os processos devem fazer a ponte entre a fachada e a camada de persistência
+ * São responsáveis pela lógica de negócio (validações, consistências, etc)
+ * Padrão: nomes de métodos em inglês
  */
 public class CepProcess {
 
@@ -17,8 +19,55 @@ public class CepProcess {
         this.dao = new CepDAO();
     }
 
-
     public List<Cep> getAll() {
         return this.dao.getAll();
+    }
+
+    public Cep setCep(Cep cep)
+    {
+        Cep cepNumeroCep = getByNumeroCep(cep.getNumeroCep());
+        if (cepNumeroCep == null)
+        {
+            return this.dao.save(cep);
+        }
+        else
+        {
+            throw new RuntimeException("Já existe um cep cadastrado com este número: "+ cep.getNumeroCep());
+        }
+    }
+
+    public Cep updateCep(Cep cep)
+    {
+        Cep cepNumeroCep = getByNumeroCep(cep.getNumeroCep());
+        if (cepNumeroCep != null && cep.getCodigoCep() == cepNumeroCep.getCodigoCep())
+        {
+            return this.dao.save(cep);
+        }
+        else
+        {
+            if (cepNumeroCep != null && (!cep.equals(cepNumeroCep) && cepNumeroCep.getCodigoCep() > 0))
+            {
+                throw new RuntimeException("O número do cep não pode ser alterado pois já existe outro cep com este número.");
+            }
+            else
+            {
+                return this.dao.save(cep);
+            }
+        }
+    }
+
+    public Cep getCep(int codigoCep)
+    {
+        return this.dao.get(codigoCep);
+    }
+
+    public boolean removeCep(int codigoCep)
+    {
+        return this.dao.remove(codigoCep);
+    }
+
+    private Cep getByNumeroCep(String numeroCep)
+    {
+        return this.dao.getByNumeroCep(numeroCep);
     }
 }
